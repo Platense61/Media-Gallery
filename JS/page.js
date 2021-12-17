e1fddbcdce40ba328079605277b27c9a57518bed
Platense61 <55705292+Platense61@users.noreply.github.com>
2021-12-17 12:39:13 -0500

    Final changes before break

--- JS/page.js
+++ JS/page.js
@@ -21,3 +21,29 @@ function showSlides(n) {
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
+
+function showModal(n){
+  var modal = document.getElementById(n);
+  modal.style.display = "block";
+}
+
+function hideModal(n){
+  var modal = document.getElementById(n);
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//mobil nav
+function navToggle() { // creates a new function named navToggle
+  var x = document.getElementById("main-nav"); // creates a variable set to #main-nav
+    if (x.className === "topnav") { // if #main-nav has a class of .topnav,
+    x.className += " responsive";   // then add a new class
+  } else {                          // if it doesn't,
+    x.className = "topnav";         // set class to .topnav
+  } //closes if-else
+} //closes function
